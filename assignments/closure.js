de34a493099e88437f2d5ca966a5532592661ecc
LD <LDinho@users.noreply.github.com>
2019-01-21 03:57:07 -0500

    MVP - add closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
-// Write a simple closure of your own creation.  Keep it simple!
-
+// Write a simple closure of your own creation. Keep it simple!
+function addTwo(n) {
+  let number = n;
+  return function() {
+    return number + 2;
+  };
+};
+const four = addTwo(4);
+console.log(four());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => {
+    count++;
+    return count;
+  }
 };
+const count = counter();
+// console.log(count);
+console.log(count());
+console.log(count());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
